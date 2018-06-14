1a7082236b0fc90904cd4bbd93e8b4308a71bc27
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-06-14 11:27:26 +0700

    added dependencies: express & body-parser

--- server/server.js
+++ server/server.js
@@ -1,4 +1,37 @@
+//import libraries
+const express = require('express')
+const bodyParser = require('body-parser')
+
+//local imports
 const {mongoose} = require('./db/mongoose')
 const {Todo} = require('./models/todo')
 const {User} = require('./models/user')
 
+//create app 
+const app = express()
+
+app.use(bodyParser.json())
+
+//create
+app.post('/todos', (request, result) => {
+    let todo = new Todo ({
+        text: request.body.text
+    })
+    todo.save().then((doc) => {
+        result.send(doc)
+    }, (error) => {
+        result.status(400).send(error)
+    })
+})
+
+// //read
+// app.get('/todos')
+// //update
+// app.post('/')
+// //delete
+// app.delete('')
+
+
+app.listen(3000, () => {
+    console.log('Started on port 3000')
+})
