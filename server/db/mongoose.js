d8e9b63e17e4172e11e0ced3f66a81774341a432
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-06-15 11:24:34 +0700

    fixed variable db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,10 @@
 //load mongoose 
 const mongoose = require('mongoose')
-const MONGODB_URI = require('./../../config')
+const {MONGODB_URI} = require('./../../config')
 // require('mongoose-type-email')
 
 //configured connection to the database
-mongoose.connect(MONGODB_URI ||'mongodb://localhost:27017/TodoApp')
+mongoose.connect({MONGODB_URI} ||'mongodb://localhost:27017/TodoApp')
 
 //export mongoose
 module.exports = {
