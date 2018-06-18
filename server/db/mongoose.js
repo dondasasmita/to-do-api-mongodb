1123d2d73845ba27e796eba2ac09f40d101ba843
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-06-18 09:39:33 +0700

    connection to mongodb URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,10 @@
 //load mongoose 
 const mongoose = require('mongoose')
-const config = require('./config')
+// const config = require('./config')
 // require('mongoose-type-email')
 
 //configured connection to the database
-mongoose.connect(config.MONGODB_URI ||'mongodb://localhost:27017/TodoApp')
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp')
 
 //export mongoose
 module.exports = {
