fdbcc57f0293eff579c63368004022fc7e8be2e6
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-08-08 23:12:37 +0700

    private routes for users

--- server/models/user.js
+++ server/models/user.js
@@ -1,69 +1,90 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 //loading validator library
-const validator = require('validator')
-const jwt = require('jsonwebtoken')
-const _ = require('lodash')
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const _ = require("lodash");
 
 const UserSchema = new mongoose.Schema({
-    username: {
+  username: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "{VALUE} is not a valid email"
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [
+    {
+      access: {
         type: String,
-        trim: true,
         required: true
-    },
-    email: {
-        type: String, 
-        required: true,
-        trim: true,
-        minlength: 1,
-        unique: true,
-        validate: {
-            validator: validator.isEmail,
-            message: '{VALUE} is not a valid email'
-        }
-    },
-    password: {
+      },
+      token: {
         type: String,
-        required: true,
-        minlength: 6
-    },
-    tokens: [{
-        access: {
-            type: String,
-            required: true
-        },
-        token: {
-            type: String,
-            required: true
-        }
-    }]
-})
+        required: true
+      }
+    }
+  ]
+});
 
 //determines what gets returned when a mongoose model converted into JSON value
-//client side will only get id, username, email 
-UserSchema.methods.toJSON = function () {
-    let user = this
-    //converts mongoose variable to regular object 
-    let userObject = user.toObject()
-    return _.pick(userObject, ['_id', 'username', 'email'])
+//client side will only get id, username, email
+UserSchema.methods.toJSON = function() {
+  let user = this;
+  //converts mongoose variable to regular object
+  let userObject = user.toObject();
+  return _.pick(userObject, ["_id", "username", "email"]);
+};
 
-}
+UserSchema.methods.generateAuthToken = function() {
+  let user = this;
+  let access = "auth";
+  let token = jwt
+    .sign({ _id: user._id.toHexString(), access }, "abc123")
+    .toString();
 
-UserSchema.methods.generateAuthToken = function () {
-    let user = this
-    let access = 'auth'
-    let token = jwt.sign({_id: user._id.toHexString(),access},'abc123').toString()
-    
-    //push and update the user model
-    // Code below had to be modified, it did not return the x-auth oh htpp headers
-    user.tokens = user.tokens.concat([{access,token}])
-    return user.save().then(() => {
-       return token
-    })
-}
+  //push and update the user model
+  // Code below had to be modified, it did not return the x-auth oh htpp headers
+  user.tokens = user.tokens.concat([{ access, token }]);
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+UserSchema.statics.findByToken = function(token) {
+  let User = this;
+  let decoded;
+
+  try {
+    decoded = jwt.verify(token, "abc123");
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  // Query
+  return User.findOne({
+    _id: decoded._id,
+    "tokens.token": token,
+    "tokens.access": "auth"
+  });
+};
 
 //defining schema for users
-const User = mongoose.model('User', UserSchema)
+const User = mongoose.model("User", UserSchema);
 
 module.exports = {
-    User
-}
+  User
+};
--- server/server.js
+++ server/server.js
@@ -1,27 +1,27 @@
 //import libraries
-const express = require('express')
-const bodyParser = require('body-parser')
-const _ = require('lodash')
+const express = require("express");
+const bodyParser = require("body-parser");
+const _ = require("lodash");
 
 //local imports
-const {ObjectID} = require('mongodb')
-const {mongoose} = require('./db/mongoose')
-const {Todo} = require('./models/todo')
-const {User} = require('./models/user')
+const { ObjectID } = require("mongodb");
+const { mongoose } = require("./db/mongoose");
+const { Todo } = require("./models/todo");
+const { User } = require("./models/user");
 
-//create app 
-const app = express()
+//create app
+const app = express();
 //setup port for deployment
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 //a function to validate ID
-const validateID = (id) => {
-    if (!ObjectID.isValid(id)) {
-        return response.status(404).send()
-    } 
-}
+const validateID = id => {
+  if (!ObjectID.isValid(id)) {
+    return response.status(404).send();
+  }
+};
 
 /*
 -----------------
@@ -30,83 +30,97 @@ TODOS ROUTES
 */
 
 //Create a todo
-app.post('/todos', (request, result) => {
-    let todo = new Todo ({
-        text: request.body.text
-    })
-    todo.save().then((doc) => {
-        result.send(doc)
-    }, (error) => {
-        result.status(400).send(error)
-    })
-})
+app.post("/todos", (request, result) => {
+  let todo = new Todo({
+    text: request.body.text
+  });
+  todo.save().then(
+    doc => {
+      result.send(doc);
+    },
+    error => {
+      result.status(400).send(error);
+    }
+  );
+});
 
-//Get all todos 
-app.get('/todos', (request, result) => {
-    Todo.find().then((todos) => {
-        result.send({todos})
-    }, (error) => {
-        result.status(400).send(error)
-    })
-})
+//Get all todos
+app.get("/todos", (request, result) => {
+  Todo.find().then(
+    todos => {
+      result.send({ todos });
+    },
+    error => {
+      result.status(400).send(error);
+    }
+  );
+});
 
 //GET todos by id
-app.get('/todos/:id', (request, response) => {
-    let id = request.params.id
-    //validate ID
-    validateID(id)
+app.get("/todos/:id", (request, response) => {
+  let id = request.params.id;
+  //validate ID
+  validateID(id);
 
-    Todo.findById(id).then((todo) => {
-        response.send(todo)
-    }, (error) => {
-        return response.status(404).send()
-    }).catch((e) => {
-        response.status(400).send()
-    })
-})
+  Todo.findById(id)
+    .then(
+      todo => {
+        response.send(todo);
+      },
+      error => {
+        return response.status(404).send();
+      }
+    )
+    .catch(e => {
+      response.status(400).send();
+    });
+});
 
 //UPDATE todos by id
-app.patch('/todos/:id', (request,result) => {
-    let id = request.params.id
-    //grabbed the request body and only allwoing the text and completed props to be update by user
-    let body = _.pick(request.body, ['text', 'completed'])
+app.patch("/todos/:id", (request, result) => {
+  let id = request.params.id;
+  //grabbed the request body and only allwoing the text and completed props to be update by user
+  let body = _.pick(request.body, ["text", "completed"]);
 
-    validateID(id)
+  validateID(id);
 
-    //get timestamp when completed is changed to true
-    if (_.isBoolean(body.completed) && body.completed) {
-        body.completedAt = new Date().getTime()
-    } else {
-        body.completed = false
-        body.completedAt = null
-    }
+  //get timestamp when completed is changed to true
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
 
-    Todo.findByIdAndUpdate(id, {$set:body}, {new: true}).then((todo) => {
-        if(!todo){
-            result.status(404).send()
-        }
-        result.status(200).send({todo})
-    }).catch((e) => {
-        result.status(400).send()
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then(todo => {
+      if (!todo) {
+        result.status(404).send();
+      }
+      result.status(200).send({ todo });
     })
-
-})
+    .catch(e => {
+      result.status(400).send();
+    });
+});
 
 // DELETE todo by id
-app.delete('/todos/:id', (request, result) => {
-    let id = request.params.id
+app.delete("/todos/:id", (request, result) => {
+  let id = request.params.id;
 
-    validateID(id)
+  validateID(id);
 
-    Todo.findByIdAndRemove(id).then((todo) => {
-        if (!todo) {
-            return result.status(404).send()
-        }
-        result.status(200).send(todo)
-    }).catch((e) => {
-        result.status(400).send()
+  Todo.findByIdAndRemove(id)
+    .then(todo => {
+      if (!todo) {
+        return result.status(404).send();
+      }
+      result.status(200).send(todo);
     })
-})
+    .catch(e => {
+      result.status(400).send();
+    });
+});
 
 /*
 -----------------
@@ -115,62 +129,86 @@ USER ROUTES
 */
 
 //GET user by id
-app.get('/users/:id', (req, res) => {
-    let id = req.params.id
+app.get("/users/:id", (req, res) => {
+  let id = req.params.id;
 
-    // validate ID
-    validateID(id)
+  // validate ID
+  validateID(id);
 
-    User.findById(id).then((user) => {
-        res.status(200).send(user)
-    }, (err) => {
-        res.status(404).send()
-    })
-})
+  User.findById(id).then(
+    user => {
+      res.status(200).send(user);
+    },
+    err => {
+      res.status(404).send();
+    }
+  );
+});
 
 //POST request to create a user
-app.post('/users', (req, res) => {
-    let body = _.pick(req.body, ['username', 'email', 'password'])
-    let user = new User (body)
+app.post("/users", (req, res) => {
+  let body = _.pick(req.body, ["username", "email", "password"]);
+  let user = new User(body);
 
-    user.save().then(() => {
-        return user.generateAuthToken();
-        }).then((token) => {
-            res.header('x-auth', token).send(user);
-        }).catch((e) => {
-            res.status(400).send(e);
-        })
-
-    //Below code does not return the header with the token (x-auth - undefined)    
-    // user.save().then(() => {
-    //     user.generateAuthToken() 
-    // }).then((token) => {
-    //     res.header('x-auth',token).send(user)
-    // }).catch((e) => {
-    //     res.status(400).send()
-    // })
-})
-
-//DELETE request 
-app.delete('/users/:id', (req, res) => {
-    let id = req.params.id
-    validateID(id)
-
-    User.findByIdAndRemove(id).then((user) => {
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.status(202).send()
-    }).catch((e) => {
-        res.status(400).send(e)
+  user
+    .save()
+    .then(() => {
+      return user.generateAuthToken();
     })
-})
+    .then(token => {
+      res.header("x-auth", token).send(user);
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
 
+  //Below code does not return the header with the token (x-auth - undefined)
+  // user.save().then(() => {
+  //     user.generateAuthToken()
+  // }).then((token) => {
+  //     res.header('x-auth',token).send(user)
+  // }).catch((e) => {
+  //     res.status(400).send()
+  // })
+});
+
+// Private Route
+app.get("/users/me", (req, res) => {
+  let token = req.header("x-auth");
+
+  User.findByToken(token)
+    .then(user => {
+      if (!user) {
+        return Promise.reject();
+      }
+      res.send(user);
+    })
+    .catch(e => {
+      res.status(401).send();
+    });
+});
+
+//DELETE request
+app.delete("/users/:id", (req, res) => {
+  let id = req.params.id;
+  validateID(id);
+
+  User.findByIdAndRemove(id)
+    .then(user => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.status(202).send();
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
+});
 
 app.listen(port, () => {
-    console.log(`Started on port ${port}`)
-})
+  console.log(`Started on port ${port}`);
+});
 
 exports.module = {
-    app
-}
+  app
+};
